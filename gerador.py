f0a85b29bc93787ceaa1b77b288012d5c30e66f1
Samuel Roberto <66800900+Bardo-programador@users.noreply.github.com>
2022-11-13 17:36:29 -0300

    Create gerador.py

---
+++ gerador.py
@@ -0,0 +1,11 @@
+from random import choice
+
+numero_digitos = int(input("quantos caracteres a senha deve ter?"))
+def gerador_de_senha(digitos):
+    caracteres_disponiveis = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%&*-+/"
+    senha = ''
+    for digitos in range(numero_digitos):
+        senha += choice(caracteres_disponiveis)
+    print(f"Sua senha é {senha}")
+    
+gerador_de_senha(numero_digitos)
