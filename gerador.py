7c2a68fab3a9aff70da7b05353edaffa7b4459eb
Samuel Roberto <66800900+Bardo-programador@users.noreply.github.com>
2022-11-17 21:59:54 -0300

    letras maiusculas adicionadas

--- gerador.py
+++ gerador.py
@@ -2,7 +2,7 @@ from random import choice
 
 numero_digitos = int(input("quantos caracteres a senha deve ter?"))
 def gerador_de_senha(digitos):
-    caracteres_disponiveis = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%&*-+/"
+    caracteres_disponiveis = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*-+/"
     senha = ''
     for digitos in range(numero_digitos):
         senha += choice(caracteres_disponiveis)
